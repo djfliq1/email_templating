a39577f5f30e9002e816d6fcae4963c789c21271
Clarence Hairston <djfliq1@gmail.com>
2019-10-03 21:17:15 -0400

    Initial Commit

--- global_variables.py
+++ global_variables.py
@@ -2,7 +2,7 @@
 
 email_greeting = "Dear %s,"
 
-email_body = "    Thank you for your interest in <anything>. Your account has been created and your\n" \
+email_body = "    Thank you for your interest in <anything>. Your account has been created and your " \
              "new login information is below:"
 
 email_time_period = "Allow for 4 to 12 hours for your account to be configured."
